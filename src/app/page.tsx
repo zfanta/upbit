2084f2a287fb65f8751a6521134efb9226e5671e
zfanta <jht008@gmail.com>
2024-01-20 16:26:08 +0900

    tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,18 @@ import useMarkets from '@/hooks/useMarkets'
 import useOrderbook from '@/hooks/useOrderbook'
 import { DataTable } from '@/components/table/data-table'
 import { useMemo } from 'react'
-import { columns } from '@/components/table/columns'
+import { columns, Data } from '@/components/table/columns'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 export default function Home () {
   const markets = useMarkets()
   const orderbook = useOrderbook(markets?.map(market => market.market) ?? ['KRW-BTC'])
   const data = useMemo(() => {
-    if (markets === undefined || orderbook === undefined) return []
+    const currencyMap = new Map<string, Data[]>()
+
+    if (markets === undefined || orderbook === undefined) {
+      return currencyMap
+    }
 
     const marketMap = new Map<string, typeof markets[number]>()
     markets.forEach(market => marketMap.set(market.market, market))
@@ -18,22 +23,44 @@ export default function Home () {
     const oderbookMap = new Map<string, typeof orderbook[number]>()
     orderbook.forEach(orderbook => oderbookMap.set(orderbook.market, orderbook))
 
-    return markets.map((market) => ({
-      id: market.market,
-      name: `${market.korean_name}(${market.english_name})`,
-      orderbook0: `${oderbookMap.get(market.market)?.orderbook_units[0].ask_price}/${oderbookMap.get(market.market)?.orderbook_units[0].bid_price}`,
-      orderbook1: `${oderbookMap.get(market.market)?.orderbook_units[1].ask_price}/${oderbookMap.get(market.market)?.orderbook_units[1].bid_price}`,
-      orderbook2: `${oderbookMap.get(market.market)?.orderbook_units[2].ask_price}/${oderbookMap.get(market.market)?.orderbook_units[2].bid_price}`,
-      orderbook3: `${oderbookMap.get(market.market)?.orderbook_units[3].ask_price}/${oderbookMap.get(market.market)?.orderbook_units[3].bid_price}`,
-      orderbook4: `${oderbookMap.get(market.market)?.orderbook_units[4].ask_price}/${oderbookMap.get(market.market)?.orderbook_units[4].bid_price}`,
-    }))
+    markets.forEach(market => {
+      const currency = market.market.split('-')[0]
+      if (!currencyMap.has(currency)) {
+        currencyMap.set(currency, [])
+      }
+
+      currencyMap.get(currency)?.push({
+        id: market.market,
+        name: `${market.korean_name}(${market.english_name})`,
+        orderbook0: `${oderbookMap.get(market.market)?.orderbook_units[0].ask_price}/${oderbookMap.get(market.market)?.orderbook_units[0].bid_price}`,
+        orderbook1: `${oderbookMap.get(market.market)?.orderbook_units[1].ask_price}/${oderbookMap.get(market.market)?.orderbook_units[1].bid_price}`,
+        orderbook2: `${oderbookMap.get(market.market)?.orderbook_units[2].ask_price}/${oderbookMap.get(market.market)?.orderbook_units[2].bid_price}`,
+        orderbook3: `${oderbookMap.get(market.market)?.orderbook_units[3].ask_price}/${oderbookMap.get(market.market)?.orderbook_units[3].bid_price}`,
+        orderbook4: `${oderbookMap.get(market.market)?.orderbook_units[4].ask_price}/${oderbookMap.get(market.market)?.orderbook_units[4].bid_price}`,
+      })
+    })
+
+    return currencyMap
   }, [markets, orderbook])
 
   return (
     <main>
-      <div className="container mx-auto py-10">
-        <DataTable columns={columns} data={data} />
-      </div>
+      <Tabs defaultValue="krw" className="container mx-auto py-10">
+        <TabsList>
+          <TabsTrigger value="krw">KRW</TabsTrigger>
+          <TabsTrigger value="btc">BTC</TabsTrigger>
+          <TabsTrigger value="usdt">USDT</TabsTrigger>
+        </TabsList>
+        <TabsContent value="krw">
+          <DataTable columns={columns} data={data.get('KRW') ?? []} />
+        </TabsContent>
+        <TabsContent value="btc">
+          <DataTable columns={columns} data={data.get('BTC') ?? []} />
+        </TabsContent>
+        <TabsContent value="usdt">
+          <DataTable columns={columns} data={data.get('USDT') ?? []} />
+        </TabsContent>
+      </Tabs>
     </main>
   )
 }
